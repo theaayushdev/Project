223c5c1585664a85b2bdb128ab9138551160bb9e
aayush dev <theaayushdev07@gmail.com>
2025-04-25 20:36:58 +0545

    issue fixed ig

--- src/pages/nutritionsection.jsx
+++ src/pages/nutritionsection.jsx
@@ -1,48 +1,48 @@
-// import React from "react";
-// import "./nutritionsection.css";
-// >>>>>>> f51e689 (hihi smoll mistake)
+import React from "react";
+import "./nutritionsection.css";
 
-// const NutritionSection = () => {
-//   const items = [
-//     {
-//       title: "What to Eat This Week",
-//       description: "Personalized weekly meal plan with balanced nutrition.",
-//     },
-//     {
-//       title: "Hydration Reminders",
-//       description: "Stay hydrated with daily water intake tips .",
-//     },
-//     {
-//       title: "Superfoods by Trimester",
-//       description: "Discover the best superfoods for each trimester.",
-//     },
-//     {
-//       title: "Doctor-Reviewed Recipes",
-//       description: "Tasty and safe meals approved by professionals.",
-//     },
-//     {
-//       title: "Printable Shopping List",
-// <<<<<<< HEAD
-//       description: "Download or print your weekly grocery list.",
-// =======
-//       description: "Download or print your weekly grocery lists.",
-// >>>>>>> f51e689 (hihi smoll mistake)
-//     },
-//   ];
 
-//   return (
-//     <div className="nutrition-section">
-//       <h2>🍽️ Nutrition Section</h2>
-//       <div className="cards-container">
-//         {items.map((item, index) => (
-//           <div className="nutrition-card" key={index}>
-//             <h3>{item.title}</h3>
-//             <p>{item.description}</p>
-//           </div>
-//         ))}
-//       </div>
-//     </div>
-//   );
-// };
+const NutritionSection = () => {
+  const items = [
+    {
+      title: "What to Eat This Week",
+      description: "Personalized weekly meal plan with balanced nutrition.",
+    },
+    {
+      title: "Hydration Reminders",
+      description: "Stay hydrated with daily water intake tips .",
+    },
+    {
+      title: "Superfoods by Trimester",
+      description: "Discover the best superfoods for each trimester.",
+    },
+    {
+      title: "Doctor-Reviewed Recipes",
+      description: "Tasty and safe meals approved by professionals.",
+    },
+    {
+      title: "Printable Shopping List",
 
-// export default NutritionSection;
+      description: "Download or print your weekly grocery lists.",
+
+      
+
+    },
+  ];
+
+  return (
+    <div className="nutrition-section">
+      <h2>🍽️ Nutrition Section</h2>
+      <div className="cards-container">
+        {items.map((item, index) => (
+          <div className="nutrition-card" key={index}>
+            <h3>{item.title}</h3>
+            <p>{item.description}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default NutritionSection;
