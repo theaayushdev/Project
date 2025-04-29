fd9436cf847d8b69dab9f2eaea8c7ba5b6dcb131
Pixiedust3 <ccsirus@gmail.com>
2025-04-29 21:11:59 +0545

    first

--- backend/app.py
+++ backend/app.py
@@ -61,7 +61,7 @@ def login():
     if not contact or not password:
         return jsonify({'error': 'Contact and password are required.'}), 400
 
-    user = User.query.filter_by(contact=contact, password=password)
+    user = User.query.filter_by(contact=contact, password=password).first()
 
     if user:
         return jsonify({'message': 'Login successful.'}), 200
