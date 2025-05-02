2c826ac9a37a82ff4670c1b4e782339571de0ce8
Pixiedust3 <ccsirus@gmail.com>
2025-05-02 22:41:15 +0545

    duedate

--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Dashboard = () => {
   return (
     <div style={{ textAlign: "center", marginTop: "5rem" }}>
-      <h1>Welcome to your Dashboard!</h1>
+    \<h1>Welcome to your Dashboard!</h1>
       <p>You have successfully logged in 🎉</p>
     </div>
   );
